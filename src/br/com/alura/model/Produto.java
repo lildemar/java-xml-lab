ce09df1292290fa50d3d318382af87517c20d797
Lildemar Matias dos Santos <lildemar.santos@pcserver.pcinformatica.com.br>
2017-07-12 18:07:05 -0300

    Lendo arquivo XML com SAX

--- src/br/com/alura/model/Produto.java
+++ src/br/com/alura/model/Produto.java
@@ -10,12 +10,23 @@ public class Produto {
 		this.preco = preco;
 	}
 
+	public Produto() {
+	}
+
 	public String getNome() {
 		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public double getPreco() {
 		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
 	}
 
 	@Override
